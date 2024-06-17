f52167ba155f91d5441dd604c440e7b5ac6203f5
Leejaeeun <149572895+leejaeeun59357@users.noreply.github.com>
2024-06-17 13:20:19 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2476 ms, Memory: 620028 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,45 +1,46 @@
-import java.io.*;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+        import java.io.*;
 
 public class Main {
-  public static final Scanner scanner = new Scanner(System.in);
-
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    // 출력을 그때 그때 하는 것보다 버퍼에 넣고 한번에 출력하기 위해 BufferedWriter를 사용
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+    BufferedWriter bw =
+            new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st =
+            new StringTokenizer(br.readLine());
+
     int N = Integer.parseInt(st.nextToken());
     int L = Integer.parseInt(st.nextToken());
+
     st = new StringTokenizer(br.readLine());
-    Deque<Node> mydeque = new LinkedList<>();
+
+    Deque<Node> deque = new LinkedList<>();
+
     for (int i = 0; i < N; i++) {
       int now = Integer.parseInt(st.nextToken());
-      // 새로운 값이 들어 올 때마다 정렬하지 않고 현재 수보다 큰 값을 덱에서 제거함으로써 시간복잡도를 줄일 수 있음
-      while (!mydeque.isEmpty() && mydeque.getLast().value > now) {
-        mydeque.removeLast();
+
+      while (!deque.isEmpty() && deque.getLast().value > now) {
+        deque.removeLast();
       }
-      mydeque.addLast(new Node(now, i));
-      // 범위에서 벗어난 값은 덱에서 제거
-      if (mydeque.getFirst().index <= i - L) {
-        mydeque.removeFirst();
+      deque.addLast(new Node(i, now));
+
+      if (deque.getFirst().index <= i - L) {
+        deque.removeFirst();
       }
-      bw.write(mydeque.getFirst().value + " ");
+      bw.write(deque.getFirst().value + " ");
     }
-    bw.flush();
-    bw.close();
+      bw.flush();
+      br.close();
   }
 
   static class Node {
-    public int value;
-    public int index;
+    int index;
+    int value;
 
-    Node(int value, int index) {
-      this.value = value;
+    Node(int index, int value) {
       this.index = index;
+      this.value = value;
     }
   }
 }
