1675af00d4959f4dae70f41e57ae9b4cbf0af474
Leejaeeun <149572895+leejaeeun59357@users.noreply.github.com>
2024-06-17 14:36:17 +0900

    [Silver II] Title: 스택 수열, Time: 1008 ms, Memory: 113808 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -3,41 +3,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
+        StringBuilder sb = new StringBuilder();
+        
         int n = sc.nextInt();
+        
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
+        
         Stack<Integer> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        int number = 1;
         boolean result = true;
+        int number = 1;
 
         for (int i = 0; i < n; i++) {
-          int currentNum = arr[i];
-
-          // 자연수가 현재 수열의 값보다 작거나 같을 때
-          if(currentNum >= number) {
-              while(currentNum >= number) {
-                  stack.push(number++);
-                  sb.append("+\n");
-              }
-              stack.pop();
-              sb.append("-\n");
-          } else {    // 현재 수열 값이 자연수보다 작을 때
-              int popValue = stack.pop();
-              if(popValue > currentNum) {
-                  System.out.println("NO");
-                  result = false;
-                  break;
-              } else {
-                  sb.append("-\n");
-              }
-          }
-
+            int currentNum = arr[i];
+            if(currentNum >= number) {
+                while(currentNum >= number) {
+                    stack.push(number);
+                    sb.append("+\n");
+                    number++;
+                }
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                int x = stack.pop();
+                if(x > currentNum) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    sb.append("-\n");
+                }
+            }
         }
+        
         if(result) {
             System.out.println(sb.toString());
         }
