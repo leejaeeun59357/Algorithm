332508f41346b2587c7219110bc35452cf3b26ea
Leejaeeun <149572895+leejaeeun59357@users.noreply.github.com>
2024-07-02 11:23:20 +0900

    [Gold III] Title: 게임 개발, Time: 244 ms, Memory: 21892 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -5,18 +5,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
+
+        int[] indegree = new int[N+1];
+        int[] buildTime = new int[N+1];
+
         ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-        
         for(int i = 0; i <= N; i++) {
             A.add(new ArrayList<>());
         }
-        
-        int[] indegree = new int[N+1];
-        int[] selfBuild = new int[N+1];
+
         for(int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            selfBuild[i] = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+            buildTime[i] = time;
             while(true) {
                 int tmp = Integer.parseInt(st.nextToken());
                 if(tmp == -1) {
@@ -26,31 +27,29 @@ public class Main {
                 indegree[i]++;
             }
         }
-        
+
         Queue<Integer> queue = new LinkedList<>();
-        
-        for(int i = 1; i<= N; i++) {
+
+        for(int i = 1; i <= N; i++) {
             if(indegree[i] == 0) {
                 queue.offer(i);
             }
         }
-        
         int[] result = new int[N+1];
-        
+
         while(!queue.isEmpty()) {
             int cur = queue.poll();
             for(int x : A.get(cur)) {
                 indegree[x]--;
-                result[x] = Math.max(result[x], result[cur] + selfBuild[cur]);
+                result[x] = Math.max(result[x], result[cur] + buildTime[cur]);
                 if(indegree[x] == 0) {
                     queue.offer(x);
                 }
             }
         }
-        
+
         for(int i = 1; i<= N; i++) {
-            System.out.println(result[i] + selfBuild[i]);
+            System.out.println(result[i] + buildTime[i]);
         }
-        
     }
 }
