f58aaeb9d498682310139bc4dde53fcab90599e1
Leejaeeun <149572895+leejaeeun59357@users.noreply.github.com>
2024-07-16 16:25:28 +0900

    [Gold II] Title: 사전, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/1256. 사전/사전.java
+++ 백준/Gold/1256. 사전/사전.java
@@ -1,70 +1,67 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	static int n,m,k;
-	static int[][] dp;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		m = sc.nextInt();
-		k = sc.nextInt();
-		
-		setDP();
-		getStr();
-		
-	}
-	
-	static void setDP() {
-		dp = new int[101][101];
-		dp[0][0] = 0;
-		for(int i=1;i<=100;i++) {
-			dp[i][0] = dp[0][i] = 1;
-		}
+    static int[][] dp = new int[101][101];
+    static int N, M, K;
 
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=m;j++) {
-				dp[i][j] = dp[i-1][j] + dp[i][j-1];
-				if(dp[i][j] > 1000000000 ) dp[i][j] = 1000000000; 
-			}
-		}
-	}
-	
-	static void getStr() {
-		int acnt = n;
-		int zcnt = m;
-		int as;
-		
-		if(k > dp[n][m]) { 
-			System.out.print(-1);
-			return;
-		}
-		
-		for(int i=0;i<n+m;i++) {
-			
-			if(acnt == 0) {
-				System.out.print("z");
-				zcnt--;
-				continue;
-			}
-			else if(zcnt == 0) {
-				System.out.print("a");
-				acnt--;
-				continue;
-			}
-			
-			as = dp[acnt-1][zcnt];
-			if( k <= as) {
-				System.out.print("a");
-				acnt--;
-			}
-			else {
-				k -= as;
-				System.out.print("z");
-				zcnt--;
-			}
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        setUp();
+        make();
+    }
+
+    private static void setUp() {
+        dp[0][0] = 0;
+        for (int i = 1; i <= 100; i++) {
+            dp[i][0] = 1;
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if (dp[i][j] > 1000000000) {
+                    dp[i][j] = 1000000000;
+                }
+            }
+        }
+    }
+
+    private static void make() {
+        int aCnt = N;
+        int zCnt = M;
+        int as;
+
+        if (K > dp[N][M]) {
+            System.out.println(-1);
+            return;
+        }
+        for (int i = 0; i < N + M; i++) {
+            if (aCnt == 0) {
+                System.out.print("z");
+                zCnt--;
+                continue;
+            } else if (zCnt == 0) {
+                System.out.print("a");
+                aCnt--;
+                continue;
+            }
+            as = dp[aCnt - 1][zCnt];
+            if (K <= as) {
+                System.out.print("a");
+                aCnt--;
+            } else {
+                K -= as;
+                System.out.print("z");
+                zCnt--;
+            }
+        }
+    }
 }
