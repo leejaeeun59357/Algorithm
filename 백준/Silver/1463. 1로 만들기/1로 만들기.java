57262c992958f8e7fb3f1f6c11b93e278622fc5d
Leejaeeun <149572895+leejaeeun59357@users.noreply.github.com>
2024-07-17 10:56:08 +0900

    [Silver III] Title: 1로 만들기, Time: 184 ms, Memory: 21632 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] D = new int[n+1];
+        D[1] = 0;
+        for(int i = 2; i <= n; i++) {
+            D[i] = D[i-1] + 1;
+            if(i% 2 == 0) {
+                D[i] = Math.min(D[i/2] + 1, D[i]);
+            }
+            if(i % 3 == 0) {
+                D[i] = Math.min(D[i/3] + 1, D[i]);
+            }
+        }
+        System.out.print(D[n]);
+    }
+}
